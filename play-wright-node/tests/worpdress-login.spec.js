e84af90f9f7ea9015189111c5811e70081b515ad
mainulsunvi <mainulsunvi@gmail.com>
2024-06-25 18:19:16 +0600

    nothing

--- play-wright-node/tests/worpdress-login.spec.js
+++ play-wright-node/tests/worpdress-login.spec.js
@@ -22,10 +22,12 @@ test.describe('WordPress Login', () => {
 			await expect(page.locator('.tf-setting-top-bar')).toBeVisible();
 		});
 		await test.step('Disable Apartment Post Types', async () => {
+			test.skip( await page.locator('#general label').filter({ hasText: /^Apartment$/ }).isChecked() == true, "Apartment Post Type is already disabled" )
+
 			await page.locator('#general label').filter({ hasText: /^Apartment$/ }).click();
 			await page.getByRole('button', { name: 'Save' }).click();
 			await page.locator('div').filter({ hasText: 'Options saved successfully!' }).isVisible();
-			await page.reload( { waitUntil: 'networkidle' } );
+			await page.reload( { waitUntil: "domcontentloaded" } );
 			await expect( page.locator("#menu-posts-tf_apartment")).not.toBeVisible();
 		});
 	});
