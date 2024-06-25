905e72f4126c9877a87cbe2ee3ecdb18de027579
mainulsunvi <mainulsunvi@gmail.com>
2024-06-25 13:53:09 +0600

    disable post type

--- play-wright-node/playwright.config.js
+++ play-wright-node/playwright.config.js
@@ -26,7 +26,7 @@ module.exports = defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://tourfic.test.msunvi/',
+    baseURL: 'http://localhost:10039',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
