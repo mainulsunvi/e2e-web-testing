df1fb75b33863138d21c8e08e7e487a8b61f1467
mainulsunvi <mainulsunvi@gmail.com>
2024-06-26 15:13:50 +0600

    global setup updated

---
+++ play-wright-node/core/global.setup.js
@@ -0,0 +1,27 @@
+import { Browser, chromium, expect, Page } from '@playwright/test';
+import playwrightConfig from '../playwright.config';
+
+async function globalSetup() {
+    const baseURL = playwrightConfig.use.baseURL;
+    const browser = await chromium.launch({ headless: false });
+    const context = await browser.newContext();
+    const page = await context.newPage();
+
+    await page.goto(`${baseURL}/wp-login.php`, { waitUntil: 'domcontentloaded' });
+    await page.locator("#user_login").fill("admin");
+    await page.locator("#user_pass").fill("admin");
+    await page.locator("#wp-submit").click();
+
+    if (await page.isVisible("#login_error")) {
+        await page.pause();
+    }
+
+    await page.waitForURL(`${baseURL}/wp-admin/`);
+    await expect(page.url(), "Your WordPress Login credentials is incorrect").toBe(`${baseURL}/wp-admin/`);
+
+    await page.context().storageState({ path: './.auth/auth.json' });
+
+    await browser.close();
+}
+
+export default globalSetup;
